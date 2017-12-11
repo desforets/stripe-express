d3888a83753abd329082c744c13f41319bf69bf4
desforets <md5@openmailbox.org>
2017-12-10 22:25:16 -0800

    handle charges! send to stripe and return the charge to the view layer

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
-const keyPublishable = "pk_test_6pRNASCoBOKtIshFeQd4XMUh"; // or const keyPublishable = process.env.PUBLISHABLE_KEY;
-const keySecret = "sk_test_BQokikJOvBiI2HlWgH4olfQ2"; // or const keySecret = process.env.SECRET_KEY;
+const keyPublishable = "pk_test_yTXzIGll1qz4bb0a19zOku70";
+const keySecret = "sk_test_qK9z9eiKlAyaskS3K1SnxvRJ";
 
 const app = require("express")();
 const stripe = require("stripe")(keySecret);
 
 const bodyParser = require('body-parser');
+const cors = require('cors')
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cors())
+app.get('/', (req, res) => res.sendStatus(200))
 
-var my_amount = {amount: 12.34, currency: "USD"};
-
-app.get("/", (req, res) =>
-  res.render("index.pug", {keyPublishable, my_amount}));
-
-app.post("/charge", (req, res) => {
-  let amount = my_amount.amount * 100;
-
-  stripe.customers.create({
-     email: req.body.stripeEmail,
-    source: req.body.stripeToken
-  })
-  .then(customer =>
-    stripe.charges.create({
-      amount,
-      description: "Sample Charge",
-         currency: my_amount.currency,
-         customer: customer.id
-    }))
-  .then(charge => res.render("charge.pug", {my_amount}));
+app.post('/test', function(req, res) {
+    console.log("Information received !");
+    console.log(req.body);
+    res.sendStatus(200);
 });
 
-app.listen(3000);
+app.post('/charge', (req, res) => {
+  const { customer, order, token } = req.body
+  stripe.customers.create({
+    email: customer.email,
+    source: token.id
+  }).then(customer =>
+    stripe.charges.create({
+      amount: order.subtotal.grandTotal * 100,
+      description: `Payment to Earth Sun for order #`,
+      statement_descriptor: 'Earth Sun ltd',
+      currency: 'cad',
+      customer: customer.id
+    }))
+  .then(charge => res.send(charge))
+  .catch(error => res.send(error))
+});
+
+app.listen(3000, () => {
+  console.log(`Express-Stripe server listenning on port 3000`)
+});
